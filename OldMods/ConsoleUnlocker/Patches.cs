047932a245f3c0b55a894d7af2c9a27c6283b2de
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2024-12-05 15:22:42 +0100

    Made ConsoleUnlocker better

--- OldMods/ConsoleUnlocker/Patches.cs
+++ OldMods/ConsoleUnlocker/Patches.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
+using UnityEngine;
 
 namespace OldMods.ConsoleUnlocker
 {
     [HarmonyPatch(typeof(PlatformSetup), "GetDevIDs")]
-    internal class PlatformSetupPatches
+    internal class DevIdPatches
     {
-        private static void Postfix()
+        public static void Postfix()
         {
+            FieldInfo DevsField = typeof(PlatformSetup).GetField("Devs", BindingFlags.NonPublic | BindingFlags.Static);
+            if (DevsField == null) return;
+            List<DevUser> Devs = DevsField.GetValue(null) as List<DevUser>;
+            DevUser me = new DevUser($"{{\"UserID\": \"{Player.Current.Network.GetNetworkId()}\", \"Flags\": 31}}");
+            MonoBehaviour.print($"Me (DevUser) | IsDev: {me.HasFeature(1)} | UnlockAll:  {me.HasFeature(2)} | MultiplayerFlag: {me.HasFeature(4)} | CommandLineAccess: {me.HasFeature(8)} | FPSDisplay: {me.HasFeature(16)}");
+            Devs.Add(me);
+            DevsField.SetValue(null, Devs);
             GlobalContext.Invoke<DevModeActivated>(Array.Empty<object>());
             MIU.Console.Instance.gameObject.SetActive(true);
         }
